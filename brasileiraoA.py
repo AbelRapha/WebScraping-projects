865bff3e5d7f08dc71b61ffd7a1afe4a9e3a44c0
Abel <abelrapha@gmail.com>
2021-09-28 02:13:38 -0300

    Create brasileiraoA.py

---
+++ brasileiraoA.py
@@ -0,0 +1,17 @@
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
+
+# Pegar o conteudo HTML a partir da URL do sofascore
+
+#Parsear o conteudo HTML com a BeutifulSoup
+
+#Estruturar conteúdo em um data frame - Pandas
+
+#Transformar Dados em um dicionário de dados próprio
+
+#Converter e salvar em um arquivo jason
